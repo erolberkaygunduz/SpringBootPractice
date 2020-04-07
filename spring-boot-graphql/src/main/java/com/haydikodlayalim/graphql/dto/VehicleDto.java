8d9638f2e5e93d4bdb15fc6a6d1634608f9effcf
Berkay Gündüz <l1321012003@stud.sdu.edu.tr>
2020-04-07 20:52:16 +0300

    Spring Practice - GraphQL Test

---
+++ spring-boot-graphql/src/main/java/com/haydikodlayalim/graphql/dto/VehicleDto.java
@@ -0,0 +1,11 @@
+package com.haydikodlayalim.graphql.dto;
+
+import lombok.Data;
+
+@Data
+public class VehicleDto {
+    private String type;
+    private String modelCode;
+    private String brandName;
+
+}
