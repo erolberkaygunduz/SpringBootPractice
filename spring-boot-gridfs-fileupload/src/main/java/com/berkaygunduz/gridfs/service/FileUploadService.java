a1b79e71563c1fc4854f22caefffab25c92c0bc8
Berkay Gündüz <l1321012003@stud.sdu.edu.tr>
2020-04-15 23:41:26 +0300

    Spring Practice - MongoDb de GridFS ile dosya yuklemek.

---
+++ spring-boot-gridfs-fileupload/src/main/java/com/berkaygunduz/gridfs/service/FileUploadService.java
@@ -0,0 +1,29 @@
+package com.berkaygunduz.gridfs.service;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public class FileUploadService {
+
+    @Autowired
+    private GridFsTemplate gridFsTemplate;
+
+    public void uploadFile(MultipartFile file) throws IOException {
+        DBObject dbObject = new BasicDBObject();
+        dbObject.put("fileName",file.getName());
+        dbObject.put("contentType", file.getContentType());
+        dbObject.put("size", file.getSize());
+        dbObject.put("userId","1234");
+        ObjectId id =gridFsTemplate.store(file.getInputStream(),file.getOriginalFilename(),dbObject);
+        System.out.println(id.toString());
+        gridFsTemplate.store(file.getInputStream(),file.getOriginalFilename(), dbObject );
+    }
+}
